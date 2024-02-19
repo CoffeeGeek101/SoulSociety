5dc05ea8cdb49efe1da30e4082b8848445cca719
CoffeeGeek101 <workwithshoumya@gmail.com>
2024-02-19 22:31:54 +0530

    setting up graphQL API

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,31 @@ import { MikroORM } from "@mikro-orm/postgresql";
 import { __prod__ } from "./constant";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
-
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { GraphQLStarter } from "./resolvers/graphStarter";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first post", fullName: "John Doe", email: "" });
-  // await orm.em.persistAndFlush(post);
-  const post = await orm.em.find(Post, {});
-  console.log(post)
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema : await buildSchema({
+      resolvers:[GraphQLStarter],
+      validate:false
+    })
+  })
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app});
+
+
+  app.listen(4000, ()=>{
+    console.log("Server started on localhost:4000");
+  })
+
 };
 
 main().catch((err) => {
